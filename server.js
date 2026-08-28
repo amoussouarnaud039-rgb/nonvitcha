2097322a624b2782d90e70c46065b57a60e6642c
amoussouarnaud039-rgb <amoussouarnaud039@gmail.com>
2026-08-28 14:56:39 +0100

    Correction erreur module http dans server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const http = http = require('http');
+const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
